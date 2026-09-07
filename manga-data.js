16b7403460f57c62c380713b4d74d2dec6157fca
tomozyo2 <tomozyo2@yahoo.co.jp>
2026-09-07 20:14:27 +0900

    4コマ漫画を更新（管理者モード） 2026/9/7 20:14:26

--- manga-data.js
+++ manga-data.js
@@ -16,8 +16,8 @@ window.MANGA_CONTENT = {
       "number": 4,
       "date": "2026年9月2日",
       "title": "",
-      "image": "manga/episode4-1788779377793.jpg",
-      "imageUpdatedAt": "1788779380067"
+      "image": "manga/episode4-1788779662047.jpg",
+      "imageUpdatedAt": "1788779664122"
     },
     "archive": [
       {
